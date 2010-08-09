2e2ac84bb1ffc8537d0fd790a9d6cb924784beda
Shriphani Palakodety <spalakod@purdue.edu>
2010-08-09 18:44:01 -0400

    This commit brings the Moattar VAD. In addition, Listener is also purely Python now.

--- audio_analysis.py
+++ audio_analysis.py
@@ -4,12 +4,12 @@
 
 #import the required modules
 import pyaudio
-import wave
-import sys
-import struct
-import numpy
 import time
+import VAD
+import wave
 import SkypeCallChecker
+import sys
+from os import name
 
 #import Skype4Py move Skype specific part of code to new class
 
@@ -23,6 +23,14 @@ try:
 except ImportError:
 	print "Growl is not installed.\n I will dump output in /var/tmp/audio.log\n"
 	Growl_exists = False
+
+
+#on unix machines, use /var/tmp/audio.log for the result.
+
+if name == "posix":
+    sys.stdout = open("/var/tmp/audio.log", "w")
+else:
+    sys.stdout = open("audio.log", "w")
 
 skype_on_call = False
 notifier = 0
@@ -60,87 +68,31 @@ def record():
 
 
 def analyze():
-    if skype_on_call:
+    if skype_on_call:  #skype is in use currently
     	print "\n"
     	print "Skype Call In Progress"
 	print "Listener On Hold"
 	return
-    inFile = wave.open("analysis.wav", "rb") #open a wav file in read mode
-    thresh = 1000  #establish a minimum threshold
-    max_samp = 0		
-    
-    decision = [0]
-
-    #for i in xrange(441):
-
-    inactive_counter = 0
-	
-    vals = inFile.readframes(inFile.getnframes()) #read in 30 samples
-    len(vals)
-    results = struct.unpack("%dh"%(inFile.getnframes()), vals)  #unpack to get the samples
-    results = [abs(x) for x in results]
-    
-    #now we need to pull 30 samples at a time (30 samples = 1 frame).
-
-    for i in xrange(4404):
-	frame = results[30*i: 30*(i+1)]
-	print frame
-	new_thresh = (thresh * (1 - (2.0 ** -7)))  +  ((2 ** -8) * max_samp)
-         
-	#check how many samples go above this new threshold
-	count = 0
-
-	for j in frame:
-	    if j > new_thresh:
-		count += 1
-	if count / 30.0 >= 0.9 :   #need it to beat 90%
-	    #frame is a candidate for speech
-	    decision.append(1)
-         
-	else:
-	    #this is where we use a counter based implementation for labelling inactiveness
-	    if inactive_counter < 10 and decision[-1] == 1: #we ignore silence for 10 runs
-		decision.append(1)
-		inactive_counter += 1
+    else:
+	#run a VAD algorithm now
+	speech =  VAD.Moattar_Homayounpour_VAD("analysis.wav", 3)
+	#speech = VAD.Milanovic_Lukac_Domazetovic_VAD("analysis.wav")
+	if speech:
+	    if Growl_exists:
+		notifier.notify('Attention','Listener', 'Speech Detected Nearby')
 	    else:
-		inactive_counter = 0
-		decision.append(0)
-         
-	#update the threshold and the max sample values
-	thresh = new_thresh
-	max_samp = max(frame)
-
-    #final check for characterization as speech, we use another counter
-    active_counter = 0 #since the inactive counter will cause silence to be recognized as speech, we only consider speech as 
-    print decision
-    final_num = 0
-    for val in decision:
-	if active_counter >= 18:
-	    print "Speech!"
-	    final_num += 1
-	    active_counter = 0
-	if val == 1:
-	    active_counter += 1
+	        print "Speech Detected Nearby!\nSomeone might be calling you"
 	else:
-	    active_counter = 0
-
-
-
-    results = [x ** 2 for x in results]
-    intensity = 20 * numpy.log10(numpy.sqrt(sum(results)/inFile.getnframes()))
+	    print "Last call detected at " + time.strftime("%H:%M:%S on %b-%d", time.gmtime())
+	
+	
     
-    if final_num >= 3 and intensity > 48:
-    	if Growl_exists:
-	    notifier.notify('Attention','Listener', 'Speech Detected Nearby')
-	else:
-	    print "Speech Detected Nearby!\nSomeone might be calling you"
-    inFile.close()
-
 if __name__ == "__main__":
+	record()
 	skypeCallChecker = SkypeCallChecker.SkypeCallChecker()
+	
 	while True:
 		if skypeCallChecker.__checkCallStatus__():
-			#print "Ma ki kir kiri\n"
 			#This essentially means that a call is taking place
 			#Right now Skype4Py isn't working as intended and I
 			#cannot fix it.
