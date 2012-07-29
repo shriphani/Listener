50cf1078fc6d9d214e90e4337526aeeaa9760186
Shriphani Palakodety <shriphanip@gmail.com>
2012-07-29 00:10:01 -0700

    VAD Bugs + Refactor
    Temporarily, Growl support is removed. I am not interested in paying
    for Growl so I will probably roll out my own way of doing notifications.

--- VAD.py
+++ VAD.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
-#Author: Shriphani Palakodety
-#A list of VAD algorithms used for Listener.
-
-
-#A list of algorithms used:
-# ->  S. Milanovic, Z. Lukac, A. Domazetovic (I cannot find their paper anywhere)
-# ->  M. H. Moattar and M. M. Homayounpour
+# Author: Shriphani Palakodety
+# spalakod@cs.cmu.edu
 
 
 #import required modules
@@ -16,242 +11,197 @@ import wave
 import struct
 
 
+MLD_FRAME_DURATION = 30 #frame length in milliseconds for milanovic, lukac and domazetovic
+MLD_SAMPLES_PER_SECOND = 44100
+MLD_SAMPLES_PER_FRAME = int(MLD_SAMPLES_PER_SECOND * (MLD_FRAME_DURATION / 1000.0))
 
-def Milanovic_Lukac_Domazetovic_VAD(filename):
-    '''Implements a simple VAD algorithm by
-       Milanovic, Lukac, Domazetovic'''
-    inFile = wave.open(filename, "rb") #open a wav file in read mode
-    thresh = 1000  #establish a minimum threshold
-    max_samp = 0		
-    
-    decision = [0]
-
-    #for i in xrange(441):
-
-    inactive_counter = 0
-	
-    vals = inFile.readframes(inFile.getnframes()) #read in 30 samples
-    #len(vals)
-    results = struct.unpack("%dh"%(inFile.getnframes()), vals)  #unpack to get the samples
-    results = [abs(x) for x in results]
-    
-    #now we need to pull 30 samples at a time (30 samples = 1 frame).
-
-    for i in xrange(4404):
-	frame = results[30*i: 30*(i+1)]
-	#print frame
-	new_thresh = (thresh * (1 - (2.0 ** -7)))  +  ((2 ** -8) * max_samp)
-         
-	#check how many samples go above this new threshold
-	count = 0
-
-	for j in frame:
-	    if j > new_thresh:
-		count += 1
-	if count / 30.0 >= 0.9 :   #need it to beat 90%
-	    #frame is a candidate for speech
-	    decision.append(1)
-         
-	else:
-	    #this is where we use a counter based implementation for labelling inactiveness
-	    if inactive_counter < 10 and decision[-1] == 1: #we ignore silence for 10 runs
-		decision.append(1)
-		inactive_counter += 1
-	    else:
-		inactive_counter = 0
-		decision.append(0)
-         
-	#update the threshold and the max sample values
-	thresh = new_thresh
-	max_samp = max(frame)
-
-    #final check for characterization as speech, we use another counter
-    active_counter = 0 #since the inactive counter will cause silence to be recognized as speech, we only consider speech as 
-    #print decision
-    final_num = 0
-    for val in decision:
-	if active_counter >= 18:
-        #print "Speech!"
-	    final_num += 1
-	    active_counter = 0
-	if val == 1:
-	    active_counter += 1
-	else:
-	    active_counter = 0
+MH_FRAME_DURATION = 10 #frame length in milliseconds for Moattar & Homayounpour
+MH_SAMPLES_PER_SECOND = 44100
+MH_SAMPLES_PER_FRAME = int(MH_SAMPLES_PER_SECOND * (MH_FRAME_DURATION / 1000.0))
 
 
+def chunk_frames_indices(samples, samples_per_frame):
+    '''
+    Args:
+        - samples: 16 bit values representing a sampled point.
 
-    results = [x ** 2 for x in results]
-    intensity = 20 * numpy.log10(numpy.sqrt(sum(results)/inFile.getnframes()))
+    Returns:
+        - an array of <FRAME_DURATION> length chunks
+    '''
+    return zip(
+        range(0, len(samples), samples_per_frame),
+        range(samples_per_frame, len(samples), samples_per_frame)
+    )
 
-    inFile.close()
+def energy(samples):
+    '''
+    Args:
+        - samples of a signal
+    '''
+    return sum([x**2 for x in samples])
 
-    if final_num >= 3 and intensity > 48:
-    	return True
-    return False
+def real_imaginary_freq_domain(samples):
+    '''
+    Apply fft on the samples and return the real and imaginary
+    parts in separate 
+    '''
+    freq_domain = fft(samples)
+    freq_domain_real = [abs(x.real) for x in freq_domain]
+    freq_domain_imag = [abs(x.imag) for x in freq_domain]
 
+    return freq_domain_real, freq_domain_imag
 
-def Moattar_Homayounpour_VAD(wave_file, duration, average_intensity, instances):
-    '''A simple VAD algorithm by Moattar and Homayounpour'''
-    inFile = wave.open("analysis.wav", "rb")
+def get_dominant_freq(real_freq_domain_part, imag_freq_domain_part):
+    '''Returns the dominant frequency'''
+    max_real = max(real_freq_domain_part)
+    max_imag = max(imag_freq_domain_part)
 
-    #set primary thresholds for energy, frequency and SFM
-    #these values were determined using experiements by the authors
-    #themselves
-    energy_prim_thresh = 40
-    freq_prim_thresh = 185
-    SFM_prim_thresh = 5
+    dominant_freq = 0
 
-    #frame details
-    frame_size = 10  #10 ms per frame.
-    samples_per_frame = int(44100 * (float(frame_size)/1000))
-    
+    if (max_real > max_imag):
+        dominant_freq = abs(fftfreq(len(real_freq_domain_part), d=(1.0/44100.0))[real_freq_domain_part.index(max_real)])
+    else:
+        dominant_freq = abs(fftfreq(len(imag_freq_domain_part), d=(1.0/44100.0))[imag_freq_domain_part.index(max_imag)])
 
-    #get all the stuff from the file.
-    vals = inFile.readframes(inFile.getnframes())
-    results = struct.unpack("%dh" %(inFile.getnframes()), vals)
+    return dominant_freq
 
-    #compute the intensity
-    intensity = 0
-    for x in results:
-        intensity += x ** 2
-    intensity = 20 * log10(sqrt(intensity/inFile.getnframes()))
+def get_freq_domain_magnitudes(real_part, imaginary_part):
+    '''Magnitudes of the real-imag frequencies'''
+    return [sqrt(x**2 + y**2) for x, y in zip(real_part, imaginary_part)]
 
-    #print results
+def get_sfm(frequencies):
+    return 10 * log10(geometric_mean(frequencies) / arithmetic_mean(frequencies))
 
-    #We record for `duration` seconds.
-    #Calculate number of frames using that
-    
-    num_frames = (duration * 1000) / frame_size
-
-    #frame attribute arrays
-    frame_energies = []  #holds the energy value for each frame
-    frame_max_frequencies = []  #holds the dominant frequency for each frame
-    frame_SFMs = []  #holds the spectral flatness measure for every frame
-    frame_voiced = []  #tells us if a frame contains silence or speech
-
-    #attributes for the entire sampled signal
-    min_energy = 0
-    min_dominant_freq = 0
-    min_sfm = 0
-
-    #a frame size here is 10 ms.
-
-    for frame_index in xrange(num_frames):
-
-        print frame_index
-        #gather values for the frames.
-        frame = results[frame_index * samples_per_frame : (frame_index+1) * samples_per_frame]
-        #frame = [abs(x) for x in frame]
-        #compute frame energy
-        energy = 0
-
-        for sample in frame:
-            energy += sample ** 2
-
-
-        #now, apply FFT on this frame. This gives us an array of complex numbers.
-        #both imaginary part and real part need to be split since they correspond to
-        #amplitudes of different frequencies in the frequency domain.
-
-        transformed = fft(frame)
-        transformed_real = [abs(x.real) for x in transformed]
-        transformed_imag = [abs(x.imag) for x in transformed]
-
-        max_real = max(transformed_real)  #contains max value of real component
-        max_imag = max(transformed_imag)  #contains max value of imag component
-
-        dominant_freq = 0 #this holds the dominant frequency component
-
-
-        #An explanation of fftfreq arguments:
-        #No. of samples = length of frame
-        #Between each frequency value, the step size is 1/44100 where 44100 Hz is the sampling
-        #frequency
-        if (max_real > max_imag):
-            dominant_freq = abs(fftfreq(len(frame), d=(1.0/44100.0))[transformed_real.index(max_real)])
-        else:
-            dominant_freq = abs(fftfreq(len(frame), d=(1.0/44100.0))[transformed_imag.index(max_imag)])
-            
-
-        #next, compute the Spectral Flatness Measure
-        #Spectral flatness measure = 10 log10(Geom_mean / Arith_mean)
-
-        frame_SFM = 0
-
-        try:
-            frame_SFM = 10 * log10(geometric_mean(frame) / arithmetic_mean(frame))
-        except ZeroDivisionError:
-            return (False, average_intensity)
-
-
-        #now, append these attributes to the frame attribute arrays created previously
-        frame_energies.append(energy)
-        frame_max_frequencies.append(dominant_freq)
-        frame_SFMs.append(frame_SFM)
-
-
-        #the first 30 frames are used to set min-energy, min-frequency and min-SFM
-        if frame_index < 30:
-	        if frame_index == 0:
-	            min_energy = energy
-	            min_dominant_freq = dominant_freq
-	            min_sfm = frame_SFM
-	        
-	        else:
-	            if energy < min_energy:
-	                min_energy = energy
-	            if dominant_freq < min_dominant_freq:
-	                min_dominant_freq = dominant_freq
-	            if frame_SFM < min_sfm:
-	                min_sfm = frame_SFM
-
-        #once we compute the min values, we compute the thresholds for each of the frame attributes
-        energy_thresh = energy_prim_thresh * log10(min_energy)
-        dominant_freq_thresh = freq_prim_thresh
-        sfm_thresh = SFM_prim_thresh
-
-        counter = 0
-
-        if (energy - min_energy) > energy_thresh:
-            counter += 1
-        if (dominant_freq - min_dominant_freq) > dominant_freq_thresh:
-            counter += 1
-        if (frame_SFM - min_sfm) > sfm_thresh:
-            counter += 1
-
-        if counter > 1:     #this means that the current frame is not silence.
-            frame_voiced.append(1)
-        else:
-            frame_voiced.append(0)
-            min_energy = ((frame_voiced.count(0) * min_energy) + energy)/(frame_voiced.count(0) + 1)
-
-        #now update the energy threshold
-        energy_thresh = energy_prim_thresh * log10(min_energy)
-
-    #once the frame attributes are obtained, a final check is performed to determine speech.
-    #at least 5 consecutive frames are needed for speech.
-
-    inFile.close()
-
-    instances += 1  #a new instance has been processed
-    old_average_intensity = average_intensity   
-    average_intensity = ((old_average_intensity * (instances-1)) + intensity) / float(instances)  #update average intensity
-
-    if locateInArray(frame_voiced, [1, 1, 1, 1, 1]) >= 0 and intensity > old_average_intensity:
-        return (True, average_intensity)
-
-    return (False, average_intensity)
-        
 def geometric_mean(frame):
     prod = 1.0
     for value in frame:
-        prod * value
+        prod *= value
 
     return prod ** (1.0/len(frame))
 
 def arithmetic_mean(frame):
     return float(sum(frame)) / float(len(frame))
+
+def get_sample_intensity(samples):
+    return 20.8 * log10(sqrt(sum([x ** 2 for x in samples])/float(len(samples))))
+
+
+class VAD(object):
+
+    @staticmethod
+    def moattar_homayounpour(wave_file, average_intensity, instances):
+        '''
+        Args:
+            - wave_file : filename containing the audio to be processes
+            - average_intensity : former average_intensity set by the user (we supply an updated value)
+            - instances : number of times this VAD was run was previously
+        '''
+        in_file = wave.open(wave_file, 'rb')
+
+        #set primary thresholds for energy, frequency and SFM
+        #these values were determined using experiements by the authors
+        #themselves
+        energy_prim_thresh = 40
+        freq_prim_thresh = 185
+        sfm_prim_thresh = 5
+        n_frames = in_file.getnframes()
+
+        samples = in_file.readframes(n_frames)
+        abs_samples = struct.unpack("%dh" % (n_frames), samples)
+
+        #compute the intensity
+        intensity = get_sample_intensity(abs_samples)
+
+        #frame attribute arrays
+        frame_energies = []  #holds the energy value for each frame
+        frame_max_frequencies = []  #holds the dominant frequency for each frame
+        frame_SFMs = []  #holds the spectral flatness measure for every frame
+        frame_voiced = []  #tells us if a frame contains silence or speech
+
+        #attributes for the entire sampled signal
+        min_energy = 0
+        min_dominant_freq = 0
+        min_sfm = 0
+
+        #check for the 30 frame mark
+        thirty_frame_mark = False
+
+        for i, frame_bounds in enumerate(chunk_frames_indices(abs_samples, MH_SAMPLES_PER_FRAME)):
+
+            frame_start = frame_bounds[0]
+            frame_end = frame_bounds[1]
+
+            # marks if 30 frames have been sampled
+            if i >= 30:
+                thirty_frame_mark = True
+
+            frame = abs_samples[frame_start:frame_end]
+    
+            #compute frame energy
+            frame_energy = energy(frame)
+            freq_domain_real, freq_domain_imag = real_imaginary_freq_domain(frame)
+            freq_magnitudes = get_freq_domain_magnitudes(freq_domain_real, freq_domain_imag)
+            dominant_freq = get_dominant_freq(freq_domain_real, freq_domain_imag)
+            frame_SFM = get_sfm(freq_magnitudes)
+
+            #now, append these attributes to the frame attribute arrays created previously
+            frame_energies.append(energy)
+            frame_max_frequencies.append(dominant_freq)
+            frame_SFMs.append(frame_SFM)
+
+            print frame_energies
+            print frame_max_frequencies
+            print frame_SFMs
+
+            #the first 30 frames are used to set min-energy, min-frequency and min-SFM
+            if not thirty_frame_mark and not i:
+                min_energy = frame_energy
+                min_dominant_freq = dominant_freq
+                min_sfm = frame_SFM
+    	        
+            elif not thirty_frame_mark:
+                min_energy = min(min_energy, frame_energy)
+                min_dominant_freq = min(dominant_freq, min_dominant_freq)
+                min_sfm = min(frame_SFM, min_sfm)
+
+            #once we compute the min values, we compute the thresholds for each of the frame attributes
+            print min_energy
+            energy_thresh = energy_prim_thresh * log10(min_energy)
+            dominant_freq_thresh = freq_prim_thresh
+            sfm_thresh = sfm_prim_thresh
+
+            counter = 0
+
+            if (frame_energy - min_energy) > energy_thresh:
+                counter += 1
+            if (dominant_freq - min_dominant_freq) > dominant_freq_thresh:
+                counter += 1
+            if (frame_SFM - min_sfm) > sfm_thresh:
+                counter += 1
+
+            if counter > 1:     #this means that the current frame is not silence.
+                frame_voiced.append(1)
+            else:
+                frame_voiced.append(0)
+                min_energy = ((frame_voiced.count(0) * min_energy) + frame_energy)/(frame_voiced.count(0) + 1)
+
+            #now update the energy threshold
+            energy_thresh = energy_prim_thresh * log10(min_energy)
+
+        #once the frame attributes are obtained, a final check is performed to determine speech.
+        #at least 5 consecutive frames are needed for speech.
+
+        in_file.close()
+
+        instances += 1  #a new instance has been processed
+        old_average_intensity = average_intensity   
+        average_intensity = ((old_average_intensity * (instances-1)) + intensity) / float(instances)  #update average intensity
+
+        if locateInArray(frame_voiced, [1, 1, 1, 1, 1]) >= 0 and intensity > old_average_intensity:
+            return (True, average_intensity)
+
+        return (False, average_intensity)
+        
 
 def locateInArray(list1, list2):
     x = 0
@@ -273,5 +223,6 @@ def locateInArray(list1, list2):
 
     
 if __name__ == "__main__":
-    #a set of tests for the VAD algorithm.
-    Moattar_Homayounpour_VAD("analysis.wav", 3)
+
+    a, b = VAD.moattar_homayounpour('analysis.wav', 0, 0)
+    print VAD.moattar_homayounpour('analysis.wav', 0, 1)
--- audio_analysis.py
+++ audio_analysis.py
@@ -2,194 +2,91 @@
 #Author: Shriphani Palakodety
 #Environment monitoring for the hearing impaired.
 
-#import the required modules
 import pyaudio
-import time
-import VAD
+import datetime
 import wave
-import SkypeCallChecker
 import sys
 import numpy
 import struct
 import os
 
-#VARIABLES
-Growl_exists = True   #variable that checks if Growl is installed. This is used for notifications.
-average_intensity = 0   #initially this sucker is 0
-instances = 0   #initially, 0 3-second samples analyzed
-lastSpeechAlertTime = time.gmtime() #record the last time speech was picked up
-
-#try to import growl
-try:
-	import Growl
-	
-except ImportError:
-	print "Growl is not installed.\n I will dump output in /var/tmp/audio.log\n"
-	Growl_exists = False
+from VAD import VAD
 
 
-#on unix machines, use /var/tmp/audio.log for the result.
+# VAD constants
+INSTANCES_VAD_IS_RUN = 0
+AVERAGE_INTENSITY_OF_RUNS = 0
+DURATION = 3   # length of 1 recording
+OUTPUT_FILE = 'analysis.wav'
 
-if os.name == "posix":
-    sys.stdout = open("/var/tmp/audio.log", "w")
-else:
-    sys.stdout = open("audio.log", "w")
+# pyaudio constants
+PYAUDIO_INSTANCE = pyaudio.PyAudio()
+PYAUDIO_CHANNELS = 1
+PYAUDIO_RATE = 44100
+PYAUDIO_INPUT = True
+PYAUDIO_FRAMES_PER_BUFFER = 1024
 
-skype_on_call = False
-notifier = 0
+# Listener constants
+NUM_FRAMES = PYAUDIO_RATE / PYAUDIO_FRAMES_PER_BUFFER
+LAST_NOTIFICATION_TIME = None
 
-#obtain a GrowlNotifier
-if Growl_exists:
-	notifier = Growl.GrowlNotifier('Listener',  ['Attention', 'test'])
-	notifier.register()
+#logging constants
+LOG_FILE_NAME = 'decisions.log'
+LOG_FILE_FD = open(LOG_FILE_NAME, 'w')
 
 def record(duration):
     '''Records Input From Microphone Using PyAudio'''
-    #duration = 3 #record for 1 second. Pretty long duration don't you think
-    outfile = "analysis.wav"
     
-    p = pyaudio.PyAudio()
-    
-    inStream = p.open(format=pyaudio.paInt16, channels=1, rate=44100,input=True, frames_per_buffer=1024)
+    in_stream = PYAUDIO_INSTANCE.open(
+        format=pyaudio.paInt16,
+        channels=PYAUDIO_CHANNELS,
+        rate=PYAUDIO_RATE,
+        input=PYAUDIO_INPUT,
+        frames_per_buffer=PYAUDIO_FRAMES_PER_BUFFER
+    )
 
     out = []
-    upper_lim = 44100 / 1024 * duration #upper limit of the range we record to. 44100 / 1024 sized chunk * 5 seconds
+    upper_lim = NUM_FRAMES * duration
     
     for i in xrange(0, upper_lim):
-        data = inStream.read(1024)
+        data = in_stream.read(1024)
         out.append(data)
     
     #now the writing section where we write to file
     data = ''.join(out)
-    outFile = wave.open(outfile, "wb")
-    outFile.setnchannels(1)
-    outFile.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-    outFile.setframerate(44100)
-    outFile.writeframes(data)
-    outFile.close()
+    out_file = wave.open(OUTPUT_FILE, "wb")
+    out_file.setnchannels(1)
+    out_file.setsampwidth(PYAUDIO_INSTANCE.get_sample_size(pyaudio.paInt16))
+    out_file.setframerate(44100)
+    out_file.writeframes(data)
+    out_file.close()
 
 
-def analyze(duration):
-    #First compare the obtained sample to every other recorded profile
-    #f = open("profiles.txt", "r")
-    #for profile in f:
-    #    if pearsonCoeff('analysis.wav', profile.strip()) > 0.8:
-    #        if Growl_exists:
-    #            notifier.notify('Attention', 'Listener', 'Profile: '+profile+'matched')
-    #        else:
-    #            print "Sound profile matched: "+profile+"\n"
-    #f.close()
-    #run a VAD algorithm now
+def analyze():
+    '''Invokes the VAD and logs the decision'''
     
-    global average_intensity
-    global instances
-    global lastSpeechAlertTime
+    global AVERAGE_INTENSITY_OF_RUNS
+    global INSTANCES_VAD_IS_RUN
+    global LAST_NOTIFICATION_TIME
 
-    (speech, avg_intensity) =  VAD.Moattar_Homayounpour_VAD("analysis.wav", duration, average_intensity, instances)
-    average_intensity = avg_intensity
-    instances += 1
-    #speech = VAD.Milanovic_Lukac_Domazetovic_VAD("analysis.wav") #uncomment to use this
+    (notify_or_not, AVERAGE_INTENSITY_OF_RUNS) =  VAD.moattar_homayounpour(OUTPUT_FILE, AVERAGE_INTENSITY_OF_RUNS, INSTANCES_VAD_IS_RUN)
 
-    #print average_intensity
-    #print instances
+    INSTANCES_VAD_IS_RUN += 1
 
-    if speech:
-        lastSpeechAlertTime = time.gmtime()
-        if Growl_exists:
-			notifier.notify('Attention','Listener', 'Speech Detected Nearby')
-        else:
-			print "Speech Detected Nearby!\nSomeone might be calling you"
-    else:
-        print "Last call detected at " + time.strftime("%H:%M:%S on %b-%d", lastSpeechAlertTime)
+    if notify_or_not:
+        dump_to_log(datetime.datetime.now())
 
+def dump_to_log(time):
+    '''The notifications module expects information of type <str:Notification> type per line'''
+    LOG_FILE_FD.write('Speech detected at: %d:%d:%d\n' % (time.hour, time.minute, time.second))
 
-
-def pearsonCoeff(signal1, signal2):
-    samples1 = wave.open(signal1, "rb")
-    samples2 = wave.open(signal2, "rb")
-    vals1 = samples1.readframes(samples1.getnframes())
-    results1 = list(struct.unpack("%dh" %(samples1.getnframes()), vals1))
-
-    vals2 = samples2.readframes(samples2.getnframes())
-    results2 = list(struct.unpack("%dh" %(samples2.getnframes()), vals2))
-    
-    #now pad the smaller array with 0s to equate in size
-    if len(results1) > len(results2):
-        results2 += [0] * (len(results1) - len(results2)) #need to pad results2
-        #print results2
-    else:
-        results1 += [0] * (len(results2) - len(results1))
-        #print results1
-    #samples_size, S1, S2, Freq_Mult = len(results1), numpy.fft.rfft(results1), numpy.fft.rfft(results2), []
-    #print samples_size
-    #for i in range(samples_size):
-    #    print i
-    #    Freq_Mult.append(S1[i].conjugate() * S2[i])
-    #
-    #corr_S1_S2 = max(numpy.fft.irfft(Freq_Mult))
-    #
-    #
-    #print ((numpy.fft.rfft(results1)),(numpy.fft.rfft((results2[::-1]))))		#correlate results1 and results2
-
-    
-    
-    corr_S1_S2 = max(abs(numpy.fft.irfft(numpy.fft.rfft(results1)*numpy.fft.rfft(results2[::-1]))))		#correlate results1 and results2
-    corr_S1_S1 = max(abs(numpy.fft.irfft(numpy.fft.rfft(results1)*numpy.fft.rfft(results1[::-1]))))		#correlate results1 and results2
-    corr_S2_S2 = max(abs(numpy.fft.irfft(numpy.fft.rfft(results2)*numpy.fft.rfft(results2[::-1]))))		#correlate results1 and results2
-   
-    samples1.close()
-    samples2.close()
-    #print "#######"
-    print corr_S1_S2
-    print corr_S1_S1
-    print corr_S2_S2
-    print "#######"
-    a = ((corr_S1_S2 ** 2.0) / (corr_S1_S1 * corr_S2_S2))
-    print numpy.sqrt(a)
-    return numpy.sqrt(a)
+def exit():
+    LOG_FILE_FD.close()
+    OUTPUT_FILE.close()
 
 
 if __name__ == "__main__":
     
-    
-    #this script is meant to be run as a daemon.
-    #fork first, and kill the parent process. 
-    #Then the child becomes a child of init.
-   
-    #pid = os.fork()
-
-    #if pid:
-        #parent process
-    #    exit()
-    #else:
-    try:
-        record(3)
-    except IOError:
-        pass    
-        
-    analyze(3)
-    skypeCallChecker = SkypeCallChecker.SkypeCallChecker()
     while True:
-        if skypeCallChecker.__checkCallStatus__():
-            #This essentially means that a call is taking place
-            #Right now Skype4Py isn't working as intended and I
-            #cannot fix it.
-            print "\n"
-            print "Skype Call In Progress"
-            print "Listener On Hold"
-            
-            #Make the app sleep for 20 seconds
-            time.sleep(20)
-        
-        else:   #Either Skype4Py is broken or there isn't a call
-            try:
-                record(3) #record for 3 seconds
-            except IOError:
-                continue 
-            analyze(3)
-            sys.stdout.flush()
-            time.sleep(1)
-		
-			
-
-
+        record(DURATION)
+        analyze()
